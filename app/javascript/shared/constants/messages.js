cc29d1f3ebde8f40dbdaaeec49e8f7633169770e
Sagar Chavda <126857566+sagarcakewalk@users.noreply.github.com>
2025-06-12 12:48:22 +0400

    feat: add support for apk file type in attachments

--- app/javascript/shared/constants/messages.js
+++ app/javascript/shared/constants/messages.js
@@ -48,7 +48,8 @@ export const ALLOWED_FILE_TYPES =
   'application/zip, application/x-7z-compressed application/vnd.rar application/x-tar,' +
   'application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/vnd.oasis.opendocument.text,' +
   'application/vnd.openxmlformats-officedocument.presentationml.presentation, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,' +
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.document,';
+  'application/vnd.openxmlformats-officedocument.wordprocessingml.document,' +
+  'application/vnd.android.package-archive, application/octet-stream,';
 
 export const ALLOWED_FILE_TYPES_FOR_TWILIO_WHATSAPP =
   'image/png, image/jpeg,' +
